e74cd1992d28efbb6a4fc5c2872956f1bc84bbae
skylineontheway <skylineontheway@hotmail.com>
2026-01-05 23:06:50 +0300

    feat: Add Vercel analytics and speed insights packages and create the root layout file.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Amiri, Noto_Sans_Arabic } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
@@ -37,8 +39,11 @@ export default function RootLayout({
           )}
         >
           {children}
+          <Analytics />
+          <SpeedInsights />
         </body>
       </html>
     </ClerkProvider>
+
   );
 }
